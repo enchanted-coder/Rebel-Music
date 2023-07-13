66df704e1fbc89749f6ee199725464da06066912
enchanted-coder <danieltogey@pm.me>
2023-07-13 19:56:48 +0300

    initial commit

---
+++ app/src/main/java/com/thenewkenya/Rebel/MainActivity.java
@@ -0,0 +1,25 @@
+package com.thenewkenya.Rebel;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.thenewkenya.Rebel.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        com.thenewkenya.Rebel.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+
+
+
+    }
+
+
+}
