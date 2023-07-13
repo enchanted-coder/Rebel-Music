6d7db15b1fe7d2732be2d9f225cb20d7fa1d5934
enchanted-coder <danieltogey@pm.me>
2023-07-13 22:20:06 +0300

    Built the UI that should work decently well

--- app/src/main/java/com/thenewkenya/Rebel/MainActivity.java
+++ app/src/main/java/com/thenewkenya/Rebel/MainActivity.java
@@ -6,16 +6,12 @@ import com.thenewkenya.Rebel.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         com.thenewkenya.Rebel.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
 
 
 
